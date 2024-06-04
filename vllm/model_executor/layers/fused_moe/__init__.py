20968de03b21142206a63b305fab4ecddd04766c
Yuan Zhou <yuan.zhou@intel.com>
2024-06-04 20:17:50 +0800

    fix
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- vllm/model_executor/layers/fused_moe/__init__.py
+++ vllm/model_executor/layers/fused_moe/__init__.py
@@ -1,8 +1,9 @@
 from vllm.model_executor.layers.fused_moe.fused_moe import (
     fused_experts, fused_moe, fused_topk, get_config_file_name)
-
+from vllm.model_executor.layers.fused_moe.static_fused_moe import static_fused_moe
 __all__ = [
     "fused_moe",
+    "static_fused_moe",
     "fused_topk",
     "fused_experts",
     "get_config_file_name",
